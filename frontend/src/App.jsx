58e312ebd79b7cd4a8a6b725623cfa0fda8c181d
Adarsh Aryan <adarsharyan2015@gmail.com>
2026-06-24 13:50:37 +0530

    React frontend setup and backend integration

---
+++ frontend/src/App.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import axios from "axios";
+
+function App() {
+
+  const [company, setCompany] = useState("");
+  const [result, setResult] = useState(null);
+
+  const handleAnalyze = async () => {
+
+  try {
+
+    const response = await axios.post(
+      "http://localhost:5000/analyze",
+      {
+        company: company
+      }
+    );
+
+    setResult(response.data);
+
+  } catch (error) {
+
+    console.log(error);
+
+  }
+};
+
+  return (
+    <div>
+      <h1>AI Investment Research Agent</h1>
+
+      <input
+        type="text"
+        placeholder="Enter company name"
+        value={company}
+        onChange={(event) => setCompany(event.target.value)}
+      />
+
+      <button onClick={handleAnalyze}>
+        Analyze
+      </button>
+
+      <p>Company: {company}</p>
+      {
+  result && (
+    <div>
+
+      <h2>Summary</h2>
+      <p>{result.summary}</p>
+
+      <h2>Final Decision</h2>
+      <p>{result.final_decision}</p>
+
+      <h2>Confidence</h2>
+      <p>{result.confidence}</p>
+
+      <h2>Reason</h2>
+      <p>{result.reason}</p>
+
+    </div>
+  )
+}
+    </div>
+  );
+}
+
+export default App;
