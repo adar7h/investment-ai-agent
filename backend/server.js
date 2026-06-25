bd0e68c6163dd9ee68f2f988b34a21a5418d8785
Adarsh Aryan <adarsharyan2015@gmail.com>
2026-06-25 20:46:52 +0530

    Integrated LangChain with Gemini API

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+const { ChatGoogleGenerativeAI } = require("@langchain/google-genai");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
 
 const app = express();
 
@@ -10,65 +10,75 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Server is running");
+  res.send("Server is running");
 });
-
 
 const PORT = 5000;
 
 app.post("/analyze", async (req, res) => {
-    try {
-        const company = req.body.company;
+  try {
+    const company = req.body.company;
 
-        const model = genAI.getGenerativeModel({
-    model: "gemini-2.5-flash"
+    const model = new ChatGoogleGenerativeAI({
+    model: "gemini-2.5-flash",
+    apiKey: process.env.GEMINI_API_KEY,
+    temperature: 0.2,
 });
 
-        const prompt = `
+    const prompt = `
 Analyze ${company} as an investment.
 
 Return ONLY valid JSON in this format:
 
 {
-"summary":"",
-"strengths":[],
-"risks":[],
-"final_decision":"",
-"confidence":0,
-"reason":""
+  "summary":"",
+  "strengths":[],
+  "risks":[],
+  "final_decision":"",
+  "confidence":0,
+  "reason":""
 }
 `;
 
-        const result = await model.generateContent(prompt);
+    const result = await model.invoke(prompt);
 
-        const response = result.response.text();
+const response = result.content;
 
-const cleanedResponse = response
-    .replace(/```json/g, "")
-    .replace(/```/g, "")
-    .trim();
+    const cleanedResponse = response
+      .replace(/```json/g, "")
+      .replace(/```/g, "")
+      .trim();
 
-try {
-    const data = JSON.parse(cleanedResponse);
-    res.json(data);
-}
-catch (error) {
+    console.log("========== RAW RESPONSE ==========");
+    console.log(cleanedResponse);
+    console.log("==================================");
+
+    try {
+      const data = JSON.parse(cleanedResponse);
+
+      res.json(data);
+
+    } catch (error) {
+
+      console.log("JSON PARSE ERROR:");
+      console.log(error);
+
+      res.status(500).json({
+        error: "Invalid JSON returned by Gemini",
+      });
+    }
+
+  } catch (error) {
+
+    console.log("GEMINI ERROR:");
     console.log(error);
 
     res.status(500).json({
-        error: "Invalid JSON returned by Gemini"
+      error: "Something went wrong",
     });
-}
-
-    } catch (error) {
-        console.log(error);
-
-        res.status(500).json({
-            error: "Something went wrong"
-        });
-    }
+  }
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
